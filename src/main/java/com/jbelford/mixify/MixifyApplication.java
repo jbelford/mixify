f7be360d98e298b26e9a512d91a3a1efd57e9fb5
Jack Belford <jackbelford12@live.ca>
2018-11-03 17:05:38 -0600

    Initial commit

---
+++ src/main/java/com/jbelford/mixify/MixifyApplication.java
@@ -0,0 +1,12 @@
+package com.jbelford.mixify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixifyApplication.class, args);
+    }
+}
